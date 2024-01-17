762164483300dd4ff4fc8363be214e68f3342d45
Alexandra Tidd <at31@sanger.ac.uk>
2024-01-17 13:29:10 +0000

    add nextflow config with max_cell_types param

--- src/01_run_scomatic.sh
+++ src/01_run_scomatic.sh
@@ -6,36 +6,36 @@ set -eo pipefail
 # wd
 wd=/lustre/scratch125/casm/team268im/at31/RA_som_mut/
 cd $wd
-work=$wd/work/
-out_dir=$wd/out/scomatic/
 config_dir=/nfs/team205/kp9/nextflow/scomatic/
-mkdir -p $work $out_dir log
+mkdir -p work/ out/scomatic/ log/
 
 # conda activate an environment which has docopt installed so jsub works
 . ~/.bashrc
 mamba activate jupy
 
 (
-    cd $out_dir
+    cd out/scomatic/
 
     # run scomatic
-    /software/team205/nextflow-23.04.1-all run /nfs/team205/kp9/nextflow/scomatic/scomatic.nf \
+    /software/team205/nextflow-23.04.1-all run \
+        /lustre/scratch125/casm/team268im/at31/nextflow/scomatic.nf \
         --projectDir $PWD \
         -params-file $config_dir/GEX.json \
         -c ../../config/run.config \
         -c $config_dir/LSF.config \
-        -w $work \
+        -w $wd/work/ \
         --location local \
         -resume
     
     # get single cell genotypes
-    /software/team205/nextflow-23.04.1-all run /nfs/team205/kp9/nextflow/scomatic/scomatic.nf \
+    /software/team205/nextflow-23.04.1-all run \
+        /lustre/scratch125/casm/team268im/at31/nextflow/scomatic.nf \
         -entry genotypes \
         --projectDir $PWD \
         -params-file $config_dir/GEX.json \
         -c ../../config/run.config \
         -c $config_dir/LSF.config \
-        -w $work \
+        -w $wd/work/ \
         --location local \
         -resume
 )
