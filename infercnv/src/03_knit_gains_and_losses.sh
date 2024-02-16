c6baaeb1f8f7d246a70ba76311e4f51ed85cb70f
Alexandra Tidd <at31@sanger.ac.uk>
2024-02-16 11:09:42 +0000

    updating gains_and_losses

--- infercnv/src/03_knit_gains_and_losses.sh
+++ infercnv/src/03_knit_gains_and_losses.sh
@@ -11,7 +11,7 @@ mamba activate jupy
 
 # knit by_celltype
 mkdir -p out/Zhang2023/by_celltype/summary/
-Rscript -e "rmarkdown::render('reports/gains_and_losses.Rmd', params = list(infercnv_dir = 'out/Zhang2023/by_celltype/', cache_dir = 'out/Zhang2023/by_celltype/summary/gains_and_losses_cache/', rerun = F), output_file = 'gains_and_losses.html', output_dir = 'out/Zhang2023/by_celltype/summary/')"
+Rscript -e "rmarkdown::render('reports/gains_and_losses.Rmd', params = list(infercnv_dir = 'out/Zhang2023/by_celltype/', cache_dir = 'out/Zhang2023/by_celltype/summary/gains_and_losses_cache/', rerun = T), output_file = 'gains_and_losses.html', output_dir = 'out/Zhang2023/by_celltype/summary/')"
 
 # # knit by_stromal_cluster
 # mkdir -p out/Zhang2023/by_stromal_cluster/summary/
