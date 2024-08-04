93a8523e67205bc15d8b1626667a82da76ce517a
alextidd <at31@sanger.ac.uk>
2024-08-04 17:38:38 +0100

    reorder scripts, trying pdf to help plot.new() issue

--- infercnv/src/01_run_infercnv_all_fibroblasts.sh
+++ infercnv/src/01_run_infercnv_all_fibroblasts.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# cd /lustre/scratch125/casm/team268im/at31/RA_som_mut/infercnv/ ; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J infercnv_all -o log "bash src/01_run_infercnv.sh"
+# cd /lustre/scratch125/casm/team268im/at31/RA_som_mut/infercnv/ ; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J infercnv_all -o log "bash src/01_run_infercnv_all_fibroblasts.sh"
 
 # run infercnv on celltypes, running all individuals together
 # TODO: decide on cluster_by_groups value
@@ -12,7 +12,8 @@
 module load singularity
 
 # run infercnv on all fibroblasts, running all individuals together
-mkdir -p out/Zhang2023/all_fibroblasts/
+# must partition work subdirectories so concomitant jobs don't cause lock issues
+mkdir -p out/Zhang2023/all_fibroblasts/ work/all_fibroblasts/
 
 # generate annotations, filter to only fibroblasts
 cat data/Zhang2023/annotations.tsv |
@@ -29,11 +30,12 @@ echo -e "id,raw_counts_matrix\nall,out/Zhang2023/sce/counts.rds" \
 # (/lustre/scratch126/casm/team268im/mp34/analysis/synovium_scRNA/infercnv_analysis.R)
 nextflow run nf-infercnv \
   --out_dir out/Zhang2023/all_fibroblasts/ \
+  -w work/all_fibroblasts/ \
   --mappings out/Zhang2023/all_fibroblasts/mappings.csv \
   --annotations out/Zhang2023/all_fibroblasts/annotations.tsv \
   --annotation_col cluster \
   --analysis_mode subclusters \
-  --cluster_by_groups FALSE \
+  --cluster_by_groups TRUE \
   --cutoff 0.1 \
   --window_length 151 \
   --noise_logistic FALSE \
@@ -43,6 +45,7 @@ nextflow run nf-infercnv \
   --HMM_report_by subcluster \
   --sd_amplifier 0.65 \
   --useRaster FALSE \
+  --output_format pdf \
   -c config/infercnv.config \
   -resume \
   -N at31@sanger.ac.uk
--- infercnv/src/02_run_infercnv_by_celltype.sh
+++ infercnv/src/02_run_infercnv_by_celltype.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# cd /lustre/scratch125/casm/team268im/at31/RA_som_mut/infercnv/ ; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J infercnv_by_celltype -o log "bash src/01_run_infercnv.sh"
+# cd /lustre/scratch125/casm/team268im/at31/RA_som_mut/infercnv/ ; bsub -q week -M 2000 -R 'select[mem>=2000] span[hosts=1] rusage[mem=2000]' -J infercnv_by_celltype -o log "bash src/02_run_infercnv_by_celltype.sh"
 
 # get singularity module
 module load singularity
 
 # run infercnv by celltype
-mkdir -p out/Zhang2023/by_celltype_test/
+mkdir -p out/Zhang2023/by_celltype_test/ 
 
 # generate annotations, differentiate mural cells and fibroblasts within stromal compartment
 cat data/Zhang2023/annotations.tsv |
@@ -36,7 +36,7 @@ nextflow run nf-infercnv \
   --annotations out/Zhang2023/by_celltype_test/annotations.tsv \
   --annotation_col celltype \
   --analysis_mode subclusters \
-  --cluster_by_groups FALSE \
+  --cluster_by_groups TRUE \
   --cutoff 0.1 \
   --window_length 151 \
   --noise_logistic FALSE \
